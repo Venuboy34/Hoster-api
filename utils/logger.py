5897f84e088f6ab6d4a9c6717327e785c60ad51a
Venuboy34 <venura505@gmail.com>
2025-11-13 12:51:01 +0530

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,29 @@
+"""
+Logging configuration
+"""
+import logging
+import sys
+from datetime import datetime
+
+
+def setup_logger():
+    """Setup application logger"""
+    
+    # Create logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    
+    # Create console handler
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    
+    # Create formatter
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    
+    return logger
